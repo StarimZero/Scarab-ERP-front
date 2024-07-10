fbb9f653e1b58dcb2ff36b7f7702fee07e41b402
nltpbj <nltpbj1725@naver.com>
2024-07-10 18:28:32 +0900

    메세지 읽기 보기 보내기

---
+++ src/components/bj/erp_message/ERP_ReadSendPage.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { IoIosArrowBack } from "react-icons/io";
+import { Card } from 'react-bootstrap';
+
+const ERP_ReadSendPage = () => {
+    const {message_id} = useParams();
+    const [list, setList] = useState([]);
+  
+    const callAPI = async() => {
+        const res= await axios.get(`/erp/sendmessage/read/${message_id}`)
+        console.log(res.data);
+        setList(res.data);
+    }
+  
+  
+    useEffect(()=>{
+      callAPI();
+    }, []);
+    
+    
+    return (
+      <div>
+        <a href='/erp/message/send'>
+            <IoIosArrowBack /> 보낸메일함
+        </a>
+        <hr/>
+        <h3>
+            {list.message_title}
+        </h3>
+        <div>
+            보낸사람: {list.message_sender}
+        </div>
+        <div className='mt-1'>
+            받은사람: {list.message_receiver}
+        </div>
+        <div className='mt-2' style={{fontSize:'10px'}}>
+            {list.message_senddate}
+        </div>
+   
+        <hr/>
+        <Card>
+            <Card.Body>
+                {list.message_content}
+            </Card.Body>
+        </Card>
+      </div>
+    )
+  }
+  
+
+export default ERP_ReadSendPage
--- src/components/bj/erp_message/ERP_insertMessagePage.jsx
+++ src/components/bj/erp_message/ERP_insertMessagePage.jsx
@@ -1,45 +1,116 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Form, Row } from 'react-bootstrap';
+import { IoIosArrowBack } from "react-icons/io";
 
 const ERP_insertMessagePage = () => {
+  const [members, setMembers] = useState([]);
 
-    const onSendmsg =async() => {
-        await axios.post('/erp/sendmessage/insert', {
-            
-        });
+
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+  const [receiver, setReceiver] = useState('');
+
+  const callAPI = async () => {
+    try {
+      const res = await axios.get('/erp/member?page=1&size=100');
+      console.log(res.data.list);
+      setMembers(res.data.list);
+
+    } catch (error) {
+      console.error('Error fetching members:', error);
     }
-    
+
+  };
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  const onSendMsg = async () => {
+    if (title === '') {
+      alert('제목을 입력하세요');
+      return;
+    }
+
+    try {
+      await axios.post('/erp/receivemessage/insert', {
+        message_sender: sessionStorage.getItem('member_info_id'),
+        message_receiver: receiver,
+        message_title: title,
+        message_content: content
+      });
+
+      alert('메세지 전송 완료');
+      window.location.href = '/erp/message';
+
+    } catch (error) {
+      console.error('뭔지는 몰라도 못보냄:', error);
+      alert('메세지 전송 실패');
+    }
+  };
+
   return (
     <div>
-    <h2 className='text-center mb-4'>메신저 쓰기</h2>
+      <div className=" mb-4">메신저 쓰기</div>
+      <a href='/erp/message'>
+            <IoIosArrowBack /> 메신저
+        </a>
+      <hr/>
+      <div className=" mb-2">
 
-    <div className='text-start mb-2'>
-             <Button className='btn-sm' variant='outline-primary'>보내기</Button>
+        <Button className="btn-sm" variant="outline-primary" onClick={onSendMsg}>
+          보내기
+        </Button>
+      </div>
+
+      <form>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column sm="1">받는 사람:</Form.Label>
+
+          <Col sm="9">
+
+            <Form.Select value={receiver} onChange={(e) => setReceiver(e.target.value)}>
+
+              <option value="">선택하세요</option>
+
+              {members && members.map(re => (
+
+                <option key={re.member_info_id} value={re.member_info_id}>
+                  {re.member_info_name} ({re.member_info_id})
+                </option>
+
+              ))}
+            </Form.Select>
+
+          </Col>
+
+
+        </Form.Group>
+
+        <Form.Group className="mt-2">
+          <Form.Control
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="mb-2"
+            rows={1}
+            style={{ width: '90%' }}
+            as="textarea"
+            placeholder="제목"/>
+
+          <Form.Control
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            as="textarea"
+            rows={10}
+            style={{ width: '90%' }}
+            placeholder="내용"/>
+
+
+        </Form.Group>
+      </form>
     </div>
+  );
+};
 
-    <Form.Control  type="file" className='mb-3' style={{width: '40%'}}/>
-
-
-       <Form>
-         <Form.Group as={Row} className="mb-3" >
-           <Form.Label column sm="1">받는 사람 :</Form.Label>
-           <Col sm="9">
-             <Form.Select>
-               <option></option>
-             </Form.Select>
-           </Col>
-         </Form.Group>
-
-         <Form.Group className='mt-2'>
-           <Form.Control as="textarea" rows={10}  style={{ width: '90%' }}/>
-
-         </Form.Group>
-       </Form>
-
-
- </div>
-  )
-}
-
-export default ERP_insertMessagePage
+export default ERP_insertMessagePage;
