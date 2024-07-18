186d406e40c94a2209c2a0c7cdbf68498f5b221f
nltpbj <167416209+nltpbj@users.noreply.github.com>
2024-07-18 18:24:32 +0900

    프론트 + 채용공고 추가 (#38)
    * message
    
    * employ type and front
    
    ---------
    
    Co-authored-by: StarimZero <164467826+StarimZero@users.noreply.github.com>

--- src/components/bj/web_company/WEB_CompanyHistoryPage.jsx
+++ src/components/bj/web_company/WEB_CompanyHistoryPage.jsx
@@ -24,14 +24,14 @@ const WEB_CompanyHistoryPage = () => {
       <div className="mt-5">
         <Row className="align-items-center">
           <Col xs={12} md={6}>
-            <img src='/images/menupage/r1.png' alt='Company Research' className="img-fluid  compnay-history-img" />
+            <img src='/images/menupage/ra1.png' alt='Company Research' className="img-fluid  compnay-history-img" />
           </Col>
           <Col xs={12} md={6}>
             <div>
               <h1 className='com-history-header'>2022</h1>
-              <p>12월) 쇠똥구리회사 상장</p>
-              <p>10월) 쇠똥구리라면 개발완료</p>
-              <p>4월) 쇠똥구리회사 노조 무력제압</p>
+              <p>12월) 삼향회사 상장</p>
+              <p>10월) 삼향라면 개발완료</p>
+              <p>4월) 삼향라면 노조 무력제압</p>
             </div>
           </Col>
         </Row>
