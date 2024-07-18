8b3297be1a5b041b24fdbb284e0cfc5252926104
jayhan1004 <astershild@naver.com>
2024-07-18 18:19:17 +0900

    검색은 완료

--- src/components/han/inventory/TradeListPage.jsx
+++ src/components/han/inventory/TradeListPage.jsx
@@ -1,23 +1,26 @@
 import { useScrollTrigger } from '@mui/material'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Button, Col, Row, Table } from 'react-bootstrap';
+import { Button, Col, Form, FormControl, InputGroup, Row, Table } from 'react-bootstrap';
 import Pagination from 'react-js-pagination';
 import '../../starim/starim_common/paging.css';
 
 const TradeListPage = () => {
     const [page, setPage] = useState(1);
-    const [size, setSize] = useState(5);
+    const [size, setSize] = useState(10);
+    const [total, setTotal] = useState(0);
     const [count, setCount] = useState(0);
+    const [key, setKey] = useState("");
+    const [word, setWord] = useState('');
     const [list, setList] = useState([]);
+    const [isSearch, setIsSearch] = useState(false);
 
     const callAPI = async()=> {
-        const res2 = await axios.get("/erp/inventory/listAlltradeTotal")
-        //console.log(res2.data);
-        setCount(res2.data);
-        const res = await axios.get(`/erp/inventory/listAlltrade?page=${page}&size=${size}`)
+        const res = await axios.get(`/erp/inventory/listAlltrade?key=${key}&word=${word}&page=${page}&size=${size}`)
         //console.log(res.data);
-        setList(res.data);
+        setCount(res.data.count);
+        setList(res.data.documents);
+        setIsSearch(key === "" && word === "");
     }
 
     useEffect(()=>{
@@ -35,7 +38,14 @@ const TradeListPage = () => {
         return amount.toLocaleString();
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        callAPI();
+        setPage(1);
+    }
+
   return (
+    <>
     <Row>
         <h1>재고리스트</h1>
         <h3 className="mb-2" onClick={()=>callAPI()} style={{cursor:'pointer'}}>전체거래내역</h3>
@@ -43,9 +53,29 @@ const TradeListPage = () => {
             <Button className='mb-2' onClick={onClickMove}>전체물품목록</Button>
         </div>
         <div>
-            <Button className='me-2' onClick={onClickMove2}>창고별물품목록</Button>
+            <Button className='me-2 mb-2' onClick={onClickMove2}>창고별물품목록</Button>
         </div>
-        <Col lg={10}>
+        <div className='mb-2'>
+            <Col lg={3}>
+                <form onSubmit={onSubmit} className='mb-2'>
+                    <InputGroup>
+                        <Form.Select value={key} onChange={(e)=>setKey(e.target.value)} style={{width:'30%'}}>
+                            <option value="date">날짜</option>
+                            <option value="type">거래분류</option>
+                            <option value="item_name">물품</option>
+                            <option value="warehouse_name">창고</option>
+                        </Form.Select>
+                        <FormControl placeholder='검색어를 입력하세요' value={word}
+                            onChange={(e)=>setWord(e.target.value)} style={{width:'55%'}}/>
+                        <Button type="submit" style={{width:'15%'}}>검색</Button>
+                    </InputGroup>
+                </form>
+            </Col>
+            {isSearch ? ("전체거래내역 : " + count + " 건") : ("검색결과 : " + count + " 건")}
+        </div>
+    </Row>
+    <Row className='justify-content-center'>
+        <Col lg={12}>
             <Table>
                 <thead className='text-center'>
                     <tr>
@@ -86,6 +116,7 @@ const TradeListPage = () => {
       }
         </Col>
     </Row>
+    </>
   )
 }
 
