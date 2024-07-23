a13f249fd3743c5e13103a35c444a16ccaba7ecb
coding-faster <162934729+coding-faster@users.noreply.github.com>
2024-07-23 18:32:03 +0900

    bbs (#53)

---
+++ src/components/jay/web_bbs/WEB_BBSPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WEB_BBSPage = () => {
+  return (
+    <div>WEB_BBSPage</div>
+  )
+}
+
+export default WEB_BBSPage
---
+++ src/routers/jay/Web_BBSRouterPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import WEB_FAQPage from '../../components/jay/web_bbs/WEB_FAQPage'
+import WEB_FAQReadPage from '../../components/jay/web_bbs/WEB_FAQReadPage'
+import WEB_BBSPage from '../../components/jay/web_bbs/WEB_BBSPage'
+import WEB_BBSReadPage from '../../components/jay/web_bbs/WEB_BBSReadPage'
+import WEB_BBSInsertPage from '../../components/jay/web_bbs/WEB_BBSInsertPage'
+
+
+
+
+const Web_BBSRouterPage = () => {
+  return (
+    <Routes>
+    
+    <Route path='faq' element={<WEB_FAQPage/>}/>
+    <Route path='bbs/read/:bbs_id' element={<WEB_BBSReadPage/>}/>
+    <Route path='bbs/insert' element={<WEB_BBSInsertPage/>}/>
+    <Route path='bbs' element={<WEB_BBSPage/>}/>
+    
+    </Routes>
+  )
+}
+
+export default Web_BBSRouterPage
