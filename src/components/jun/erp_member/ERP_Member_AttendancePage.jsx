068356ffdfa5fbbbf66d0431e1eea5a405735e32
Simon-JunhoLee <leejunho0722@naver.com>
2024-07-30 16:20:32 +0900

    Junho (#80)
    * 수정
    
    * 찐찐최종
    
    ---------
    
    Co-authored-by: StarimZero <164467826+StarimZero@users.noreply.github.com>

--- src/components/jun/erp_member/ERP_Member_AttendancePage.jsx
+++ src/components/jun/erp_member/ERP_Member_AttendancePage.jsx
@@ -11,15 +11,9 @@ const ERP_Member_AttendancePage = () => {
     const member_info_name = sessionStorage.getItem('member_info_name');
     const member_info_id = sessionStorage.getItem("member_info_id");
 
-    const [member, setMember] = useState({});
+
     const [attendanceList, setAttendanceList] = useState([]);
-
-    const callMember = async () => {
-        const url = `/erp/member/${member_info_id}`;
-        const res = await axios.get(url);
-        setMember(res.data);
-    }
-
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const callAttendanceList = async () => {
         const url = `/erp/attendance/listMember/${member_info_key}`;
         const res = await axios.get(url);
@@ -28,7 +22,6 @@ const ERP_Member_AttendancePage = () => {
 
     useEffect(() => {
         callAttendanceList();
-        callMember();
     }, []);
 
     const getTileContent = ({ date, view }) => {
@@ -36,9 +29,13 @@ const ERP_Member_AttendancePage = () => {
             const formattedDate = moment(date).format('YYYY-MM-DD');
             const attendanceToday = attendanceList.find(({ member_attendance_date }) => member_attendance_date === formattedDate);
             if (attendanceToday) {
-                return <div className="tile-attended">출근</div>;
+                return (
+                    <div className="tile-attended">
+                        <div className="attendance-dot"></div>출근
+                    </div>
+                );
             } else {
-                return <div className="tile-absent"></div>;
+                return <div className="tile-absent">&nbsp;</div>;
             }
         }
     }
@@ -51,9 +48,13 @@ const ERP_Member_AttendancePage = () => {
             <div className="px-5 calendar-container">
                 <Calendar
                     tileContent={getTileContent}
-                    className="calendar"
-                    showNavigation={true}
+                    onChange={setSelectedDate}
+                    value={selectedDate}
+                    calendarType="gregory"
                     view="month"
+                    prev2Label={null}
+                    next2Label={null}
+                    showNeighboringMonth={false}
                 />
             </div>
         </Row>
